d692d6af13cdbb0a3a8320d89f453c8a6cd00853
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-11-15 13:49:20 +0100

    Started working on CutOpenEvent

--- Assets/scripts/Event.cs
+++ Assets/scripts/Event.cs
@@ -4,7 +4,7 @@ class Event : MonoBehaviour {
     bool active;
     protected Body body;
 
-	public Event(){}
+    public Event() {}
     
     public Event(Body body_) {
         body = body_;
@@ -22,10 +22,47 @@ class Event : MonoBehaviour {
 
 //Each separate event subclass should be placed in it's own script,
 //    on a certain part of the body
-
-
 class CutOpenEvent : Event {
-	public CutOpenEvent(Body body_) {
-		body = body_;
-	}
+    const int STATE_NEEDS_BACTERIAL = 0;
+    const int STATE_CUTTING = 0;
+    int state;
+    
+    float bacJelAmount; //in cl
+    const float BACJEL_ADDED_PER_SECOND = 0.2;
+    float bacJelAmountNeeded;
+    
+    public CutOpenEvent(Body body_) {
+            body = body_;
+            state = STATE_NEEDS_BACTERIAL;
+            bacJelAmount = 0.0F;
+            bacJelAmountNeeded = 2.0F + 1.0*body.numPlayers;
+    }
+        
+    void Update() {
+        if (state == STATE_NEEDS_BACTERIAL) {
+            Fiducial bacJelFid = body.fs[body.BACTERIALJEL];
+            if (bacJelFid.active) {
+                bacJelAmount += BACJEL_ADDED_PER_SECOND * Time.deltaTime;
+                
+                if (bacJelAmount > bacJelAmountNeeded) {
+                    state = STATE_CUTTING;
+                    return;
+                }
+                
+                drawNeedsBacterial();
+            }
+        } else if (state == STATE_CUTTING) {
+            drawIsCutting();
+        }
+        
+        float bloodDiff = -1.0F;
+        body.bloodAmount += bloodDiff;
+        Debug.Log("CutOpenEvent changed bloodAmount with" + bloodDiff + "(" + bloodAmount + "cl left");
+    }
+    
+    void drawNeedsBacterial() {
+    }
+    
+    void drawIsCutting() {
+    }
 }
