6d0c2069e6f158e3039af10cf48c735e79685816
Niclas Ericsson <niclas.ericsson@live.se>
2015-12-03 16:43:53 +0100

    Added texture changer

---
+++ Assets/ChangeTexture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTexture : MonoBehaviour {
+
+	public Texture[] textures;
+	public int currentTexture;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown ("p")) {
+			currentTexture++;
+			currentTexture %= textures.Length;
+			GetComponent<Renderer>().material.mainTexture = textures[currentTexture];
+		}
+	
+	}
+}
