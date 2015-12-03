a1f54df75bde34c1d0539ccd840cc288bde62568
Niclas Ericsson <niclas.ericsson@live.se>
2015-12-03 13:36:29 +0100

    Added Organ Interaction, rigidbody madness

---
+++ Assets/scripts/OrganInteraction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrganInteraction : MonoBehaviour {
+
+	void OnMouseDown(){
+		if (gameObject.GetComponent<Rigidbody> () == null) {
+			Rigidbody gameObjectsRigidBody = gameObject.AddComponent<Rigidbody> ();
+		}
+	}
+
+	void OnMouseDrag(){
+		float dist = transform.position.z - Camera.main.transform.position.z;
+		Vector3 pos = Input.mousePosition;
+		pos.z = dist;
+		pos = Camera.main.ScreenToWorldPoint(pos);
+		pos.y = transform.position.y;
+		transform.position = pos;
+	}
+}
